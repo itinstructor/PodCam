4033a5b31888d68365c061e13b37bf6386d5c377
William Loring <32806514+itinstructor@users.noreply.github.com>
2025-10-31 19:31:36 -0600

    Remove api_key_ts.py from version control

--- api_key_ts.py
+++
@@ -1,7 +0,0 @@
-# thingspeak_api_key.py
-# ThingSpeak channel write api key
-
-# GoPiGo Sensor Channel
-THINGSPEAK_API_KEY = '2PSR26046W1ZWGVC'
-
-THINGSPEAK_URL = 'http://api.thingspeak.com/update'
