4cdfd0aef0a83fbe4bfeb6cc81f31d0f07c028d6
William Loring <32806514+itinstructor@users.noreply.github.com>
2025-11-28 14:27:00 -0700

    Update

--- co2_sensor_ts.py
+++ co2_sensor_ts.py
@@ -7,6 +7,8 @@ import adafruit_scd4x
 import time
 
 TEMPERATURE_OFFSET = -0.01
+
+
 class CO2Sensor:
     """Reader for SCD4x CO2 sensor via I2C."""
 
@@ -17,22 +19,12 @@ class CO2Sensor:
         self.humidity = None
         self.i2c = board.I2C()
         self.scd4x = adafruit_scd4x.SCD4X(self.i2c)
-        print("Serial number:", [hex(i) for i in self.scd4x.serial_number])
         self.scd4x.start_periodic_measurement()
-        print("Waiting for first measurement...")
         time.sleep(2)
-        # Throw away the first 5 readings to get sensor warmed up
-        for _ in range(5):
-            if self.scd4x.data_ready:
-                self.co2 = self.scd4x.CO2
-                self.temp_c = self.scd4x.temperature
-                self.temp_c += TEMPERATURE_OFFSET
-                self.temp_f = self.temp_c * 9.0 / 5.0 + 32.0
-                self.humidity = self.scd4x.relative_humidity
-
 
     def read_sensors(self):
-        """Read CO2, temperature (C, F), and humidity from the sensor. Returns tuple or None."""
+        """Read CO2, temperature (C, F), and humidity from the sensor.
+           Returns tuple or None."""
         if self.scd4x.data_ready:
             self.co2 = self.scd4x.CO2
             self.temp_c = self.scd4x.temperature
